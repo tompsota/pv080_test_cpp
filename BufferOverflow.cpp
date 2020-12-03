e71ff1e130b9320ab7d709eebe4619aa7522a39f
tompsota <to.psota@gmail.com>
2020-12-03 16:57:45 +0100

    fix 3

--- BufferOverflow.cpp
+++ BufferOverflow.cpp
@@ -36,13 +36,13 @@ void demoBufferOverflowData() {
 	printf("login as: ");
 	fflush(stdout);
 	//gets(userName); // use scanf("%s", userName); if gets fails with identifier not found
-	scanf("%s", userName);
+	scanf_s("%s", userName);
 
 	// Get password
 	printf("%s@vulnerable.machine.com: ", userName);
 	fflush(stdout);
 	//gets(passwd);  
-	scanf("%s", passwd); // use scanf("%s", passwd); if gets fails with identifier not found
+	scanf_s("%s", passwd); // use scanf("%s", passwd); if gets fails with identifier not found
 
 	// Check user rights (set to NORMAL_USER and not changed in code)
 	if (userRights == NORMAL_USER) {
